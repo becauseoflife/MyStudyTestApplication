08383b3646061dbbbf06d6eb5c72a4809f420c22
becauseoflife <1752196851@qq.com>
2019-09-29 23:10:56 +0800

    消息对话框 例3-5

--- app/src/main/java/com/jnu/student/ChapterThreeActivity.java
+++ app/src/main/java/com/jnu/student/ChapterThreeActivity.java
@@ -2,7 +2,10 @@ package com.jnu.student;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
@@ -11,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,12 +25,17 @@ public class ChapterThreeActivity extends AppCompatActivity {
 /*    private Button btn;
     TextView MenuTxt;*/
 /*    上下文菜单 例3-4*/
+/*
     TextView MenuTxt1, MenuTxt2, MenuTxt3;
     private static final int item1 = Menu.FIRST;
     private static final int item2 = Menu.FIRST+1;
     private static final int item3 = Menu.FIRST+2;
     private String[] str = {"张凯朝", "李昊翔", "李彦锋"};
-
+*/
+/*消息对话框 例3-5*/
+    ProcessBuilder myDialog;
+    Button btn_common_box, btn_input_box;
+    LinearLayout login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,10 +74,10 @@ public class ChapterThreeActivity extends AppCompatActivity {
             startActivity(intent);
         }*/
 
-        /*选项菜单 例3-3*/
+/*选项菜单 例3-3*/
 /*        MenuTxt = findViewById(R.id.textView2);*/
-        /*    上下文菜单 例3-4*/
-        MenuTxt1 = findViewById(R.id.Menu_text_view1);
+/*    上下文菜单 例3-4*/
+/*        MenuTxt1 = findViewById(R.id.Menu_text_view1);
         MenuTxt2 = findViewById(R.id.Menu_text_view2);
         MenuTxt3 = findViewById(R.id.Menu_text_view3);
         MenuTxt1.setText(str[0]);
@@ -76,9 +85,75 @@ public class ChapterThreeActivity extends AppCompatActivity {
         MenuTxt3.setText(str[2]);
         registerForContextMenu(MenuTxt1);
         registerForContextMenu(MenuTxt2);
-        registerForContextMenu(MenuTxt3);
+        registerForContextMenu(MenuTxt3);*/
+/*    消息对话框 例3-5*/
+     btn_common_box = findViewById(R.id.botton_common_box);
+     btn_input_box = findViewById(R.id.button_input_box);
+     btn_common_box.setOnClickListener(new mClick());
+     btn_input_box.setOnClickListener(new mClick());
     }
-/*选项菜单 例3-3*/
+
+    private class mClick implements View.OnClickListener {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(ChapterThreeActivity.this);
+        @Override
+        public void onClick(View view) {
+            if (view == btn_common_box){
+                // 设置对话框标题
+                dialog.setTitle("警告");
+                // 设置对话框图标
+                dialog.setIcon(R.drawable.img);
+                // 设置对话框显示内容
+                dialog.setMessage("本项操作可能导致信息泄露!");
+                // 设置对话框的“确定”按钮
+                dialog.setPositiveButton("确定", new okClick());
+                // 创建对话框对象
+                dialog.create();
+                // 显示对话框
+                dialog.show();
+            }
+            else if (view == btn_input_box){
+                // 从另外组件中关联组件
+                login = (LinearLayout)getLayoutInflater().inflate(R.layout.login, null);
+                dialog.setTitle("用户登录").setMessage("请输入用户名和密码").setView(login);
+                dialog.setPositiveButton("确定", new loginClick());
+                dialog.setNegativeButton("退出", new exitClick());
+                dialog.setIcon(R.drawable.img);
+                dialog.create();
+                dialog.show();
+            }
+        }
+        // 普通对话框确定按钮事件
+        private class okClick implements DialogInterface.OnClickListener {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        }
+        // 输入对话框 “确定” 按钮事件
+        private class loginClick implements DialogInterface.OnClickListener {
+            EditText pwdTxt;
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                pwdTxt = login.findViewById(R.id.editText_login_password);
+                // 取出输入框的值与密码“admin”比较
+                if (pwdTxt.getText().toString().equals("admin")){
+                    Toast.makeText(getApplicationContext(), "登录成功", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "密码错误", Toast.LENGTH_SHORT).show();
+                }
+                dialogInterface.dismiss();
+            }
+        }
+        // 输入对话框“取消”按钮事件
+        private class exitClick implements DialogInterface.OnClickListener {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                ChapterThreeActivity.this.finish();
+            }
+        }
+    }
+
+    /*选项菜单 例3-3*/
 /*    public boolean onCreateOptionMenu(Menu menu){
         // 调用父类方法加入菜单系统
         super.onCreateOptionsMenu(menu);
@@ -109,15 +184,15 @@ public class ChapterThreeActivity extends AppCompatActivity {
         }
         return true;
     }*/
-
-    /*    上下文菜单，通过长按条目触发*/
-    public void onCreateContextMenu(ContextMenu menu, View view,  ContextMenu.ContextMenuInfo menuInfo){
+/*    上下文菜单 例3-4*/
+/*    上下文菜单，通过长按条目触发*/
+/*    public void onCreateContextMenu(ContextMenu menu, View view,  ContextMenu.ContextMenuInfo menuInfo){
        menu.setHeaderTitle("人物简介");
        menu.add(0, item1, 0,"年龄");
        menu.add(0, item2, 0, "财富");
        menu.add(0, item3, 0,"恋爱史");
     }
-    /*菜单单击响应*/
+    *//*菜单单击响应*//*
     public boolean onContextItemSelected(MenuItem item){
         // 获取当前被选择的菜单项信息
         switch (item.getItemId()){
@@ -132,5 +207,6 @@ public class ChapterThreeActivity extends AppCompatActivity {
                 break;
         }
         return true;
-    }
+    }*/
+
 }
